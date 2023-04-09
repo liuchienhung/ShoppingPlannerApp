d49097401ea514b38fd5fe7e601f2c284fdea60a
DESKTOP-1RD78J6\neil <neil@geosat.com.tw>
2023-04-10 00:55:00 +0800

    Add new shopping item.

--- app/src/main/java/ShoppingListAdapter.kt
+++ app/src/main/java/ShoppingListAdapter.kt
@@ -5,14 +5,25 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppingplannerapp.R
 
-class ShoppingListAdapter(private val items: List<ShoppingItem>) :
+class ShoppingListAdapter(private val items: MutableList<ShoppingItem>) :
     RecyclerView.Adapter<ShoppingListAdapter.ViewHolder>() {
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val itemName: TextView = itemView.findViewById(R.id.item_name)
         val itemQuantity: TextView = itemView.findViewById(R.id.item_quantity)
         val itemPrice: TextView = itemView.findViewById(R.id.item_price)
         val itemTotalPrice: TextView = itemView.findViewById(R.id.item_total_price)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                addItem(items[position])
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -30,4 +41,11 @@ class ShoppingListAdapter(private val items: List<ShoppingItem>) :
     }
 
     override fun getItemCount(): Int = items.size
+
+    fun addItem(item: ShoppingItem) {
+        items.add(item)
+        notifyItemInserted(items.size - 1)
+    }
+
 }
+
--- app/src/main/java/com/example/shoppingplannerapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingplannerapp/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.shoppingplannerapp
 
 import ShoppingItem
 import ShoppingListAdapter
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var items: MutableList<ShoppingItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val items = listOf(
+        val items = mutableListOf(
             ShoppingItem("牛奶", 2, 10.0),
             ShoppingItem("麵包", 1, 5.5),
             ShoppingItem("蘋果", 3, 3.0)
@@ -27,4 +32,28 @@ class MainActivity : AppCompatActivity() {
         val adapter = ShoppingListAdapter(items)
         recyclerView.adapter = adapter
     }
+    fun addItem(view: View) {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_add_item)
+
+        val etName = dialog.findViewById<EditText>(R.id.et_item_name)
+        val etQuantity = dialog.findViewById<EditText>(R.id.et_item_quantity)
+        val etPrice = dialog.findViewById<EditText>(R.id.et_item_price)
+        val btnCancel = dialog.findViewById<Button>(R.id.btn_cancel)
+        val btnAdd = dialog.findViewById<Button>(R.id.btn_add_item)
+
+        btnCancel.setOnClickListener { dialog.dismiss() }
+        btnAdd.setOnClickListener {
+            val name = etName.text.toString()
+            val quantity = etQuantity.text.toString().toInt()
+            val price = etPrice.text.toString().toDouble()
+            val item = ShoppingItem(name, quantity, price)
+            items.add(item)
+            (recyclerView.adapter as ShoppingListAdapter).notifyItemInserted(items.size - 1)
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
 }
